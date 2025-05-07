c99d18c69a0ce0762277b66b85abcbca8813f494
amarkatoliya <amarbharavad994@gmail.com>
2025-05-07 10:33:40 +0530

    all models added

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,15 +1,15 @@
-export const userRolesEnum =  {
+export const UserRolesEnum =  {
     ADMIN : "admin",
     PROJECT_ADMIN : "project_admin",
     MEMBER : "member"
 };
 
-export const AvailableUserRoles = Object.values(userRolesEnum);
+export const AvailableUserRoles = Object.values(UserRolesEnum);
 
-export const taskStatusEnum = {
+export const TaskStatusEnum = {
     TODO: "todo",
     IN_PROGRESS: "in_progress",
     DONE: "done",
 };
 
-export const AvailableTaskStatus = Object.values(taskStatusEnum);
+export const AvailableTaskStatus = Object.values(TaskStatusEnum);
